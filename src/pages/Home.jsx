d8f68de4b5b3a91354100c878f3ba8acde3afb94
Rodrigo <rodri.crzz@gmail.com>
2025-08-11 09:21:47 -0300

    arreglos Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-// pages/Home.jsx
+import { Link } from "react-router-dom";
 import banner from "../assets/RowaStudioBanner2.jpg";
 
 export default function Home() {
@@ -11,12 +11,12 @@ export default function Home() {
           <h2>Estética avanzada para tu bienestar</h2>
           <p>Descubrí nuestros servicios y productos exclusivos</p>
           <div className="d-flex justify-content-center gap-3 mt-4">
-            <a href="/tienda" className="btn btn-primary btn-lg">
+            <Link to="/tienda" className="btn btn-primary btn-lg">
               Ir a la tienda
-            </a>
-            <a href="/servicios" className="btn btn-outline-light btn-lg">
+            </Link>
+            <Link to="/servicios" className="btn btn-outline-light btn-lg">
               Conocer servicios
-            </a>
+            </Link>
           </div>
         </div>
       </div>
